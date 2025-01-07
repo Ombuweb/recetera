d88435deec788ffe1c97cf714f15a356285f47a9
Ombuweb <tjiheron@gmail.com>
2025-01-07 12:27:36 +0100

    renamed

--- core/contracts/services/recipe-services.contract.ts
+++
@@ -1,9 +0,0 @@
-import type { Recipe } from '~/core/models/domain';
-
-export interface IRecipeService {
-  getRecipes(): Promise<Recipe[]>;
-  getRecipe(id: string): Promise<Recipe>;
-  createRecipe(recipe: Recipe): Promise<Recipe>;
-  updateRecipe(recipe: Recipe): Promise<Recipe>;
-  deleteRecipe(id: string): Promise<void>;
-}
