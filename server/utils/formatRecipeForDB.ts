c6ea1d7e9b17a6d7657db7a0f25bb03fe7d52fdd
Ombuweb <tjiheron@gmail.com>
2025-02-07 00:09:12 +0100

    extract data from form data

---
+++ server/utils/formatRecipeForDB.ts
@@ -0,0 +1,19 @@
+import { RecipeModel } from '~/core/models/domain';
+import type { MultiPartData } from 'h3';
+
+export default function formatRecipeFormDataForDB(
+  recipeFormData: MultiPartData[] | undefined
+) {
+  return recipeFormData?.reduce((acc, value) => {
+    const key = value.name as keyof RecipeModel;
+    const arrayBuffer = value.data as Buffer<ArrayBufferLike>;
+    if (key === 'tags') {
+      const array = new Uint8Array(arrayBuffer);
+      const text = new TextDecoder().decode(array);
+      acc[key] = text.split(',');
+    } else {
+      acc[key] = value.data.toString();
+    }
+    return acc;
+  }, {} as RecipeModel) as RecipeModel;
+}
