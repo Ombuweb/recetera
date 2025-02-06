90957570600f76baf89c8fa89cde917d640c732c
Ombuweb <tjiheron@gmail.com>
2025-02-07 00:10:33 +0100

    fix

--- server/api/recipe/create.post.ts
+++ server/api/recipe/create.post.ts
@@ -1,22 +1,12 @@
 import { RecipeModel } from '~/core/models/domain';
 import { getRecipeDatabaseService } from '~/server/utils/database/locator';
+import formatRecipeFormDataForDB from '~/server/utils/formatRecipeForDB';
 
 export default defineEventHandler(async (event) => {
   const db = getRecipeDatabaseService();
   const recipeFormData = await readMultipartFormData(event);
 
-  const recipe = recipeFormData?.reduce((acc, value) => {
-    const key = value.name as keyof RecipeModel;
-    const arrayBuffer = value.data as Buffer<ArrayBufferLike>;
-    if (key === 'tags') {
-      const array = new Uint8Array(arrayBuffer);
-      const text = new TextDecoder().decode(array);
-      acc[key] = text.split(',');
-    } else {
-      acc[key] = value.data.toString();
-    }
-    return acc;
-  }, {} as RecipeModel) as RecipeModel;
+  const recipe = formatRecipeFormDataForDB(recipeFormData) as RecipeModel;
   console.log('Creating recipe:', recipe);
   const createdRecipe = await db.createRecipe(recipe);
   return createdRecipe.id;
