43158c752c43ad8efa2af0c970dc4482c767e652
Ombuweb <tjiheron@gmail.com>
2025-03-10 11:00:42 +0100

    feat: getRecipesByUser

--- core/contracts/services/recipe-database-service.contract.ts
+++ core/contracts/services/recipe-database-service.contract.ts
@@ -1,8 +1,13 @@
 import type { RecipeModel } from '~/core/models/domain';
 
 export interface IRecipeDatabaseService {
+  // postRecipe(recipe: RecipeModel): Promise<RecipeModel>;
+  // putRecipe(recipe: RecipeModel): Promise<RecipeModel>;
+  // deleteRecipe(id: string): Promise<void>;
+
   getRecipes(): Promise<RecipeModel[]>;
   getRecipe(id: string): Promise<RecipeModel>;
+  getRecipesByUser(user_id: number): Promise<RecipeModel[]>;
   createRecipe(recipe: RecipeModel): Promise<string>;
   updateRecipe(recipe: RecipeModel): Promise<RecipeModel>;
   deleteRecipe(id: string): Promise<void>;
