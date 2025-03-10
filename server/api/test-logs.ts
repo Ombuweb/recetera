eb449b322fd8887901508d489d43e35db755e5de
Ombuweb <tjiheron@gmail.com>
2025-03-10 11:01:37 +0100

    update

--- server/api/test-logs.ts
+++ server/api/test-logs.ts
@@ -1,8 +1,9 @@
 // import { getReceteraLoggingService } from '../utils/database/locator';
-
+let counter = 0;
 export default defineEventHandler(async (event) => {
   // getReceteraLoggingService().logInfo('Hello from Grafana, recetera here');
   // return {
   //   body: 'Hello from Grafana',
   // };
+  console.log('Hello from Grafana, recetera here', counter++);
 });
