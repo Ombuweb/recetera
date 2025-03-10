353b1fb8428353bfa97321f34bba0d2c004c692b
Ombuweb <tjiheron@gmail.com>
2025-03-10 11:00:08 +0100

    clean up

--- server/utils/database/sql/drizzle.ts
+++ server/utils/database/sql/drizzle.ts
@@ -1,13 +1,7 @@
-import { drizzle } from 'drizzle-orm/d1';
 export { sql, eq, and, or, like } from 'drizzle-orm';
-
 import * as schema from './schema';
 
 export const tables = schema;
 
-export function useDrizzle() {
-  return drizzle(hubDatabase(), { schema });
-}
-export type DrizzleInstance = ReturnType<typeof useDrizzle>;
-
 export type Recipe = typeof schema.recipes.$inferSelect;
+export type User = typeof schema.users.$inferSelect;
